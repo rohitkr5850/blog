79e8e348fdbc8e7cc12af742e50d00a611aba417
rohitkr5850 <rroy854100@gmail.com>
2025-01-10 23:32:57 +0530

    Y

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogPage from "./components/BlogPage";
+
+const App = () => {
+  return (
+    <div>
+      <BlogPage />
+    </div>
+  );
+};
+
+export default App;
